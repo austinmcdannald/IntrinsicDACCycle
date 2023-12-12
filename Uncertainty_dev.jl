4f10e51a3928a3787d97541f4b29e8a6c1168cf5
Austin McDannald <austin.mcdannald@nist.gov>
2023-12-12 15:34:18 -0500

    Update to uncertainty development

--- Uncertainty_dev.jl
+++ Uncertainty_dev.jl
@@ -11,6 +11,9 @@ using Pkg
 Pkg.activate("C:/Users/asm6/.julia/dev/IntrinsicDACCycle")
 # Pkg.instantiate("C:/Users/asm6/.julia/dev/IntrinsicDACCycle")
 
+# ╔═╡ 96b977eb-09e4-47d0-a44f-e84754e44bcf
+Pkg.add("UMAP")
+
 # ╔═╡ 39adca47-0916-40bf-9a68-ccca916286bc
 using Revise
 
@@ -26,6 +29,9 @@ using Statistics
 # ╔═╡ 1190b5f5-1ae9-49d0-a6e1-631f315a779f
 using Plots
 
+# ╔═╡ 9679c9dc-b2cb-4542-a11c-ba152d252fcd
+using UMAP
+
 # ╔═╡ 9b4bdf3e-f56a-46f4-9b12-181791871870
 #Pkg.activate("/users/asm6/.Julia/dev/IntrinsicDACCycle")
 
@@ -37,8 +43,8 @@ Base_directory = "C:/Users/asm6/Documents/Projects/DAC/Results"
 
 # ╔═╡ 38d44cae-df73-4dd1-bdc2-f4498fffd17f
 begin
-	name = "OKILEA_clean"
-	# name = "acs.cgd.5b01554_VAGNUP1452791_clean"
+	# name = "OKILEA_clean"
+	name = "acs.cgd.5b01554_VAGNUP1452791_clean"
 	# name = "CUCKIV_charged"
 end
 
@@ -91,6 +97,27 @@ Results_dict = IntrinsicDACCycle.Optimize_Intrinsic_Refresh(Base_directory, name
 # ╔═╡ aa5da8a4-ee18-435c-a5b4-57c278907511
 Results_dict_2 = IntrinsicDACCycle.Optimize_Intrinsic_Refresh_w_err(Base_directory, name, α)
 
+# ╔═╡ 99ebe87f-cf56-4647-b489-f9f2416adc23
+plot(Results_dict["Refresh_Path"]["Temperatures"], Results_dict["Refresh_Path"]["Pressures"])
+
+# ╔═╡ 980e56de-6d31-4fa7-856a-71e85f52a3d9
+plot(Results_dict_2["Refresh_Path"]["Temperatures"], Results_dict_2["Refresh_Path"]["Pressures"])
+
+# ╔═╡ 92513c81-1076-4443-a651-858cde1485a5
+begin
+	dT_max = 0.25
+	for i in 0:20
+		T1 = 200.0
+		ΔT = 200.0
+		dT_max_i = dT_max*0.75^i   
+		thing = T1:dT_max_i:T1+ΔT
+		@show length(thing)
+	end
+end
+
+# ╔═╡ b515a8f1-af51-4414-8b4f-41641b0aa8d2
+
+
 # ╔═╡ Cell order:
 # ╠═fea28170-74cb-11ee-052f-cd5041d01722
 # ╠═9b4bdf3e-f56a-46f4-9b12-181791871870
@@ -101,6 +128,8 @@ Results_dict_2 = IntrinsicDACCycle.Optimize_Intrinsic_Refresh_w_err(Base_directo
 # ╠═79e0e799-7652-4647-b3e6-e38b50936eee
 # ╠═0562ebed-0c40-48ef-87a7-9aaf9d129141
 # ╠═1190b5f5-1ae9-49d0-a6e1-631f315a779f
+# ╠═96b977eb-09e4-47d0-a44f-e84754e44bcf
+# ╠═9679c9dc-b2cb-4542-a11c-ba152d252fcd
 # ╠═35ce9d94-7205-4322-b6f2-0016e74895c5
 # ╠═38d44cae-df73-4dd1-bdc2-f4498fffd17f
 # ╠═92ee65e1-e046-42da-bf20-ab4dd0abbbcf
@@ -113,3 +142,7 @@ Results_dict_2 = IntrinsicDACCycle.Optimize_Intrinsic_Refresh_w_err(Base_directo
 # ╠═34ddcf3d-5f69-4fd1-93db-882c5c209c30
 # ╠═0ede4183-46d2-47ea-9a66-29191cea7549
 # ╠═aa5da8a4-ee18-435c-a5b4-57c278907511
+# ╠═99ebe87f-cf56-4647-b489-f9f2416adc23
+# ╠═980e56de-6d31-4fa7-856a-71e85f52a3d9
+# ╠═92513c81-1076-4443-a651-858cde1485a5
+# ╠═b515a8f1-af51-4414-8b4f-41641b0aa8d2
